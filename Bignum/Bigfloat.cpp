b3918ba9604f26fdfa0f84768e2f1b02f520366b
Nikolay Fedorov <nianfedorov@edu.hse.ru>
2024-02-13 16:02:21 +0300

    Add a header and some implementations of class methods

--- Bignum/Bigfloat.cpp
+++ Bignum/Bigfloat.cpp
@@ -1,1 +1,17 @@
 #include "Bigfloat.h"
+
+Bigfloat::Bigfloat(double value, int precision) {
+    for (int i = 0; i < precision; ++i) {
+        value *= 10;
+    }
+    _value = Bigint(static_cast<int64_t>(value));
+    _precision = precision;
+}
+
+Bigint Bigfloat::get_value() const {
+    return _value;
+}
+
+uint8_t Bigfloat::get_precision() const {
+    return _precision;
+}
--- Bignum/Bigfloat.h
+++ Bignum/Bigfloat.h
@@ -1,10 +1,41 @@
 #ifndef HSE_BIGNUM_LIBRARY_BIGFLOAT_H
 #define HSE_BIGNUM_LIBRARY_BIGFLOAT_H
-
+#include "Bigint.h"
 
 class Bigfloat {
+public:
+    explicit Bigfloat(double value = 0.0, int precision = 6);
+    explicit Bigfloat(const std::string &value, int precision = 6);
 
+    [[nodiscard]] Bigint get_value() const;
+    [[nodiscard]] uint8_t get_precision() const;
+    [[nodiscard]] std::string to_string() const;
+
+    Bigfloat operator-() const;
+
+    Bigfloat operator+(const Bigfloat &other) const;
+    Bigfloat operator-(const Bigfloat &other) const;
+    Bigfloat operator*(const Bigfloat &other) const;
+    Bigfloat operator/(const Bigfloat &other) const;
+
+    Bigfloat &operator+=(const Bigfloat &other);
+    Bigfloat &operator-=(const Bigfloat &other);
+    Bigfloat &operator*=(const Bigfloat &other);
+    Bigfloat &operator/=(const Bigfloat &other);
+
+    bool operator==(const Bigfloat &other) const;
+    bool operator!=(const Bigfloat &other) const;
+    bool operator<(const Bigfloat &other) const;
+    bool operator>(const Bigfloat &other) const;
+    bool operator<=(const Bigfloat &other) const;
+    bool operator>=(const Bigfloat &other) const;
+
+    friend std::ostream &operator<<(std::ostream &os, const Bigfloat &bigfloat);
+    friend std::istream &operator>>(std::istream &is, Bigfloat &bigfloat);
+
+private:
+    Bigint _value;
+    uint8_t _precision;
 };
-
 
 #endif
