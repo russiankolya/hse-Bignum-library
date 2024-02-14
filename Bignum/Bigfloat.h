9ae227453f9be5083bf77c5042aa380f426a6193
Nikolay Fedorov <nianfedorov@edu.hse.ru>
2024-02-14 20:58:52 +0300

    Add a header for the Bigfloat class

--- Bignum/Bigfloat.h
+++ Bignum/Bigfloat.h
@@ -3,7 +3,40 @@
 #include "Bigint.h"
 
 class Bigfloat {
+public:
+    Bigfloat(std::string number = 0, uint8_t precision = 6);
 
+
+    [[nodiscard]] Bigint number() const;
+    [[nodiscard]] uint8_t precision() const;
+
+    [[nodiscard]] std::string to_string() const;
+
+    Bigfloat operator-() const;
+
+    Bigfloat operator+(const Bigfloat& other) const;
+    Bigfloat operator-(const Bigfloat& other) const;
+    Bigfloat operator*(const Bigfloat& other) const;
+    Bigfloat operator/(const Bigfloat& other) const;
+
+    Bigfloat operator+=(const Bigfloat& other);
+    Bigfloat operator-=(const Bigfloat& other);
+    Bigfloat operator*=(const Bigfloat& other);
+    Bigfloat operator/=(const Bigfloat& other);
+
+    bool operator==(const Bigfloat& other) const;
+    bool operator!=(const Bigfloat& other) const;
+    bool operator<(const Bigfloat& other) const;
+    bool operator>(const Bigfloat& other) const;
+    bool operator<=(const Bigfloat& other) const;
+    bool operator>=(const Bigfloat& other) const;
+
+
+private:
+    Bigint _number;
+    uint8_t _precision;
+
+    Bigfloat(Bigint number = Bigint(0), uint8_t precision = 6);
 };
 
 #endif
