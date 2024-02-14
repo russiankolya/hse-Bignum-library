694a98aa9f2892f2b8aebf062caf32e4d4d30a12
Nikolay Fedorov <nianfedorov@edu.hse.ru>
2024-02-14 23:50:21 +0300

    Add bigint constructor, string constructor, to_string method and tests for them

--- Bignum/Bigfloat.cpp
+++ Bignum/Bigfloat.cpp
@@ -1,1 +1,88 @@
 #include "Bigfloat.h"
+
+Bigfloat::Bigfloat(Bigint number, uint8_t precision) {
+    _number = number;
+    _precision = precision;
+}
+
+Bigfloat::Bigfloat(std::string number, uint8_t precision) : _precision(precision) {
+    bool is_negative = false;
+    if (number[0] == '-') {
+        number = number.substr(1);
+        is_negative = true;
+    }
+
+    size_t dot_position = number.find('.');
+
+    if (std::any_of(number.begin(), number.end(), [](char c) { return !std::isdigit(c) && c != '.'; })
+        || std::count(number.begin(), number.end(), '.') > 1
+        || number.empty()
+        || (number.size() > 1 && number[0] == '0' && number[1] == '0')
+        || dot_position == 0 || dot_position == number.size() - 1) {
+        throw std::runtime_error("Invalid number format");
+    }
+
+    if (dot_position == std::string::npos) {
+        _number = Bigint(number);
+    } else {
+        std::string integer_part = number.substr(0, dot_position);
+        std::string fractional_part = number.substr(dot_position + 1);
+
+        uint8_t current_precision = fractional_part.size();
+        while (current_precision < precision) {
+            fractional_part += '0';
+            current_precision++;
+        }
+        while (current_precision > precision) {
+            fractional_part.pop_back();
+            current_precision--;
+        }
+
+        //remove leading zeros from fractional part
+        std::reverse(fractional_part.begin(), fractional_part.end());
+        while (fractional_part.back() == '0') {
+            fractional_part.pop_back();
+        }
+        std::reverse(fractional_part.begin(), fractional_part.end());
+
+
+        std::string result;
+
+        if (integer_part != "0") {
+            result += integer_part;
+        }
+        result += fractional_part;
+
+        _number = Bigint(result);
+    }
+
+    if (_number == Bigint("0")) {
+        is_negative = false;
+    }
+
+    if (is_negative) {
+        _number = -_number;
+    }
+}
+
+Bigint Bigfloat::number() const {
+    return _number;
+}
+
+uint8_t Bigfloat::precision() const {
+    return _precision;
+}
+
+std::string Bigfloat::to_string() const {
+    if (_precision == 0) {
+        return _number.to_string();
+    }
+    std::string result = _number.value();
+    if (result.size() <= _precision) {
+        result.insert(0, _precision - result.size(), '0');
+        result.insert(0, "0.");
+    } else {
+        result.insert(result.size() - _precision, ".");
+    }
+    return _number.is_negative() ? "-" + result : result;
+}
--- Google_tests/BigfloatTest.cpp
+++ Google_tests/BigfloatTest.cpp
@@ -1,2 +1,131 @@
 #include "gtest/gtest.h"
 #include "Bigfloat.h"
+
+// Default constructor test
+TEST(BigfloatTest, testDefaultConstructor) {
+    Bigfloat bigfloat;
+    ASSERT_EQ(bigfloat.number().value(), "0");
+    ASSERT_EQ(bigfloat.precision(), 6);
+}
+
+
+
+// String constructor test
+TEST(BigfloatTest, testStringConstructor1) {
+    Bigfloat bigfloat("123.456789", 6);
+    ASSERT_EQ(bigfloat.number().to_string(), "123456789");
+    ASSERT_EQ(bigfloat.precision(), 6);
+}
+
+TEST(BigfloatTest, testStringConstructor2) {
+    Bigfloat bigfloat("123.45", 6);
+    ASSERT_EQ(bigfloat.number().to_string(), "123450000");
+    ASSERT_EQ(bigfloat.precision(), 6);
+}
+
+TEST(BigfloatTest, testStringConstructor3) {
+    Bigfloat bigfloat("123.4567", 2);
+    ASSERT_EQ(bigfloat.number().to_string(), "12345");
+    ASSERT_EQ(bigfloat.precision(), 2);
+}
+
+TEST(BigfloatTest, testStringConstructor4) {
+    Bigfloat bigfloat("123.4567", 8);
+    ASSERT_EQ(bigfloat.number().to_string(), "12345670000");
+    ASSERT_EQ(bigfloat.precision(), 8);
+}
+
+TEST(BigfloatTest, testStringConstructor5) {
+    Bigfloat bigfloat("0.12345678", 8);
+    ASSERT_EQ(bigfloat.number().to_string(), "12345678");
+    ASSERT_EQ(bigfloat.precision(), 8);
+}
+
+TEST(BigfloatTest, testStringConstructor6) {
+    Bigfloat bigfloat("-0.12345678", 8);
+    ASSERT_EQ(bigfloat.number().to_string(), "-12345678");
+    ASSERT_EQ(bigfloat.precision(), 8);
+}
+
+TEST(BigfloatTest, testStringConstructor7) {
+    Bigfloat bigfloat("-0.12345678", 2);
+    ASSERT_EQ(bigfloat.number().to_string(), "-12");
+    ASSERT_EQ(bigfloat.precision(), 2);
+}
+
+TEST(BigfloatTest, testStringConstructor8) {
+    Bigfloat bigfloat("1234.5678", 2);
+    ASSERT_EQ(bigfloat.number().to_string(), "123456");
+    ASSERT_EQ(bigfloat.precision(), 2);
+}
+
+TEST(BigfloatTest, testStringConstructor9) {
+    Bigfloat bigfloat("1234.5678", 8);
+    ASSERT_EQ(bigfloat.number().to_string(), "123456780000");
+    ASSERT_EQ(bigfloat.precision(), 8);
+}
+
+TEST(BigfloatTest, testStringConstructor10) {
+    ASSERT_ANY_THROW(Bigfloat("00.31312"));
+}
+
+TEST(BigfloatTest, testStringConstructor11) {
+    ASSERT_ANY_THROW(Bigfloat(".31312"));
+}
+
+TEST(BigfloatTest, testStringConstructor12) {
+    ASSERT_ANY_THROW(Bigfloat("31.31.2"));
+}
+
+
+
+/// to_string method test
+TEST(BigfloatTest, testToString1) {
+    Bigfloat bigfloat("123.456789", 6);
+    ASSERT_EQ(bigfloat.to_string(), "123.456789");
+}
+
+TEST(BigfloatTest, testToString2) {
+    Bigfloat bigfloat("123.45", 6);
+    ASSERT_EQ(bigfloat.to_string(), "123.450000");
+}
+
+TEST(BigfloatTest, testToString3) {
+    Bigfloat bigfloat("123.4567", 2);
+    ASSERT_EQ(bigfloat.to_string(), "123.45");
+}
+
+TEST(BigfloatTest, testToString4) {
+    Bigfloat bigfloat("0.4567", 8);
+    ASSERT_EQ(bigfloat.to_string(), "0.45670000");
+}
+
+TEST(BigfloatTest, testToString5) {
+    Bigfloat bigfloat("-0.001", 8);
+    ASSERT_EQ(bigfloat.to_string(), "-0.00100000");
+}
+
+TEST(BigfloatTest, testToString6) {
+    Bigfloat bigfloat("-1.1", 2);
+    ASSERT_EQ(bigfloat.to_string(), "-1.10");
+}
+
+TEST(BigfloatTest, testToString7) {
+    Bigfloat bigfloat("0", 2);
+    ASSERT_EQ(bigfloat.to_string(), "0.00");
+}
+
+TEST(BigfloatTest, testToString8) {
+    Bigfloat bigfloat("-0", 2);
+    ASSERT_EQ(bigfloat.to_string(), "0.00");
+}
+
+TEST(BigfloatTest, testToString9) {
+    Bigfloat bigfloat("1234.5678", 0);
+    ASSERT_EQ(bigfloat.to_string(), "1234");
+}
+
+TEST(BigfloatTest, testToString10) {
+    Bigfloat bigfloat("-0", 0);
+    ASSERT_EQ(bigfloat.to_string(), "0");
+}
