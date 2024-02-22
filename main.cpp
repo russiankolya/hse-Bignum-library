47eb81fa17e6790e3e912bcbe2242eca5ebb6d57
Nikolay Fedorov <nianfedorov@edu.hse.ru>
2024-02-22 14:34:44 +0300

    Add user input for precision

--- main.cpp
+++ main.cpp
@@ -2,28 +2,26 @@
 #include "Bigfloat.h"
 #include <chrono>
 
-const int PRECISION = 101;
-
-Bigfloat calculate() {
-    Bigfloat pi("0", PRECISION);
+Bigfloat calculate(int precision) {
+    Bigfloat pi("0", precision);
 
     uint8_t k = 0;
-    Bigfloat degree_of_16 = Bigfloat("1", PRECISION);
-    Bigfloat cur_8k_value = Bigfloat("0", PRECISION);
+    Bigfloat degree_of_16 = Bigfloat("1", precision);
+    Bigfloat cur_8k_value = Bigfloat("0", precision);
     while (true) {
-        Bigfloat a1 = Bigfloat("4", PRECISION) / (cur_8k_value + Bigfloat("1", PRECISION));
-        Bigfloat a2 = Bigfloat("2", PRECISION) / (cur_8k_value + Bigfloat("4", PRECISION));
-        Bigfloat a3 = Bigfloat("1", PRECISION) / (cur_8k_value + Bigfloat("5", PRECISION));
-        Bigfloat a4 = Bigfloat("1", PRECISION) / (cur_8k_value + Bigfloat("6", PRECISION));
+        Bigfloat a1 = Bigfloat("4", precision) / (cur_8k_value + Bigfloat("1", precision));
+        Bigfloat a2 = Bigfloat("2", precision) / (cur_8k_value + Bigfloat("4", precision));
+        Bigfloat a3 = Bigfloat("1", precision) / (cur_8k_value + Bigfloat("5", precision));
+        Bigfloat a4 = Bigfloat("1", precision) / (cur_8k_value + Bigfloat("6", precision));
 
         Bigfloat sum = (a1 - a2 - a3 - a4) / degree_of_16;
 
-        if (sum > Bigfloat("0", PRECISION)) {
+        if (sum > Bigfloat("0", precision)) {
             pi += sum;
             ++k;
 
-            cur_8k_value += Bigfloat("8", PRECISION);
-            degree_of_16 *= Bigfloat("16", PRECISION);
+            cur_8k_value += Bigfloat("8", precision);
+            degree_of_16 *= Bigfloat("16", precision);
         } else {
             break;
         }
@@ -32,15 +30,14 @@ Bigfloat calculate() {
 }
 
 int main() {
-    Bigfloat a = "1.234"_bf;
-
-    std::cout << a << "\n";
+    int precision;
+    std::cin >> precision;
 
     auto start = std::chrono::high_resolution_clock::now();
-    Bigfloat pi = calculate();
+    Bigfloat pi = calculate(precision);
     auto end = std::chrono::high_resolution_clock::now();
 
-    std::cout << "First " << PRECISION << " of pi after decimal point is: " << pi << "\n";
+    std::cout << "First " << precision << " of pi after decimal point is: " << pi << "\n";
     std::cout << "Execution time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
               << "ms\n";
 }
